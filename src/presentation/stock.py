efc8fb6d10d8c404ec02db45b2db4e4578b22c4e
mabittar <ma_bittar@yaho.com.br>
2024-08-08 20:29:43 -0300

    fix(stock): adjust path

--- src/presentation/stock.py
+++ src/presentation/stock.py
@@ -5,12 +5,12 @@ router = APIRouter(tags=["Stock"])
 
 
 @router.get(
-    path="/stock/{stock}",
+    path="/stock/{stock_symbol}",
     response_class=JSONResponse,
     name="stock",
     status_code=status.HTTP_200_OK,
 )
 async def check_health(
-    *, stock: str = Path(description="Stock Symbol")
+    *, stock_symbol: str = Path(description="Stock Symbol")
 ) -> JSONResponse:
-    return JSONResponse(content={"stock_symbol": stock})
+    return JSONResponse(content={"stock_symbol": stock_symbol})
