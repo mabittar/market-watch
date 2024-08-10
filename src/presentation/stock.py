b9258d59523ea13fdffa6baabf22d1a873be3bcf
mabittar <ma_bittar@yaho.com.br>
2024-08-10 18:27:56 -0300

    feat(stock): create stock operation schema and route

--- src/presentation/stock.py
+++ src/presentation/stock.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Path, status
+from fastapi import APIRouter, HTTPException, Path, status
 from fastapi.responses import JSONResponse
 
 from ..application.stock_application import StockApplication
-from ..models.schema.stock import Stock
+from ..application.create_stock_operation_application import CreateStockOperation
+from ..models.schema.stock import StockOperation, StockOperationReponse, StockResponse
 
 
 router = APIRouter(tags=["Stock"])
@@ -11,12 +12,34 @@ router = APIRouter(tags=["Stock"])
 @router.get(
     path="/stock/{stock_symbol}",
     response_class=JSONResponse,
-    name="stock",
+    name="get stock",
     status_code=status.HTTP_200_OK,
 )
-async def check_health(
+async def get_stock(
     *, stock_symbol: str = Path(description="Stock Symbol")
-) -> Stock:
+) -> StockResponse:
     if stock_symbol.__len__() < 2:
-        raise ValueError
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="stock_symbol must be greater than 2 letters",
+        )
     return await StockApplication().execute(stock_symbol)
+
+
+@router.post(
+    path="/stock/{stock_symbol}",
+    response_class=JSONResponse,
+    name="create stock operations",
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_stock_operation(
+    *, stock_symbol: str = Path(description="Stock Symbol"), operation: StockOperation
+) -> StockOperationReponse:
+    if stock_symbol.__len__() < 2:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="stock_symbol must be greater than 2 letters",
+        )
+    return await CreateStockOperation().execute(
+        stock_symbol=stock_symbol, amount=operation.amount
+    )
